8dc8db6caec2da9a863c86be1319b3534b109f92
AlisaP8 <78349377+AlisaP8@users.noreply.github.com>
2022-01-04 19:10:58 +0400

    user_gender

--- authapp/forms.py
+++ authapp/forms.py
@@ -1,11 +1,11 @@
 import hashlib
 import random
 
-from django.contrib.auth.forms import UserCreationForm,AuthenticationForm,UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, UserChangeForm
 from django.core.exceptions import ValidationError
 from django import forms
 
-from authapp.models import User
+from authapp.models import User, UserProfile
 from authapp.validator import validate_name
 
 
@@ -14,7 +14,7 @@ class UserLoginForm(AuthenticationForm):
     # username = forms.CharField(widget=forms.TextInput(),validators=[validate_name])
     class Meta:
         model = User
-        fields = ('username','password')
+        fields = ('username', 'password')
 
     def __init__(self, *args, **kwargs):
         super(UserLoginForm, self).__init__(*args, **kwargs)
@@ -59,18 +59,33 @@ class UserRegisterForm(UserCreationForm):
 
 class UserProfilerForm(UserChangeForm):
     # first_name = forms.CharField(widget=forms.TextInput(),validators=[validate_name])
-    image = forms.ImageField(widget=forms.FileInput(),required=False)
+    image = forms.ImageField(widget=forms.FileInput(), required=False)
     age = forms.IntegerField(widget=forms.NumberInput(), required=False)
 
     class Meta:
         model = User
-        fields = ('username','email','first_name','last_name','image','age')
+        fields = ('username', 'email', 'first_name', 'last_name', 'image', 'age')
 
     def __init__(self, *args, **kwargs):
         super(UserProfilerForm, self).__init__(*args, **kwargs)
         self.fields['username'].widget.attrs['readonly'] = True
         self.fields['email'].widget.attrs['readonly'] = True
 
-        for field_name , field in self.fields.items():
+        for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control py-4'
         self.fields['image'].widget.attrs['class'] = 'custom-file-input'
+
+
+class UserProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        exclude = ('user',)
+
+    def __init__(self, *args, **kwargs):
+        super(UserProfileEditForm, self).__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            if field_name != 'gender':
+                field.widget.attrs['class'] = 'form-control py-4'
+            else:
+                field.widget.attrs['class'] = 'form-control'
