6dea8380caf5e63fd00fce027ba37fe92f86597a
AlisaP8 <78349377+AlisaP8@users.noreply.github.com>
2022-02-22 13:44:44 +0400

    add_cache

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -234,3 +234,16 @@ if DEBUG:
 
 # if os.name == 'posix':
 #     <настройки memcashed>
+
+CACHE_MIDDLEWARE_ALIAS = 'default'
+CACHE_MIDDLEWARE_SECONDS = 120
+CACHE_MIDDLEWARE_KEY_PREFIX = 'geekbrains'
+
+CACHE = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
+
+LOW_CACHE = True
